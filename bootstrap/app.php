fcd8506727c58469a09b033cef394aa83526170a
ShamanKing19 <gera.sukhomlin@gmail.com>
2024-12-02 16:35:46 +0200

    api + todo

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,10 +3,12 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api/v1.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -14,5 +16,13 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        // TODO: Вернуть, когда будет фронт
+//        $exceptions->render(function (\Throwable $e, Request $request) {
+//            if ($request->is('api/*')) {
+//                return response()->json([
+//                    'message' => $e->getMessage(),
+//                    'trace' => $e->getTrace()
+//                ], 404);
+//            }
+//        });
     })->create();
