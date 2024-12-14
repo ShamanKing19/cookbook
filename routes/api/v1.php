2af6cf2bf4ff7e7e6759128ba09971a01ba8fa02
ShamanKing19 <gera.sukhomlin@gmail.com>
2024-12-14 20:59:39 +0200

    fix

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('/v1')->group(function () {
     Route::get('/meals', [MealsController::class, 'list']);
     Route::get('/meals/{id}', [MealsController::class, 'showById'])->whereNumber('id');
-    Route::get('/meals/{slug}', [MealsController::class, 'showBySlug'])->whereAlpha('slug');
+    Route::get('/meals/{slug}', [MealsController::class, 'showBySlug'])->where('slug', '[\w-]+');
     Route::post('/meals', [MealsController::class, 'createOrUpdate']);
 });
